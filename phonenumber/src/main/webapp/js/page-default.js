e9d8263cf358d7ef6ef67c14d3b0f8ef87f908ef
etandon <eshantandon@gmail.com>
2016-01-22 07:36:48 -0500

    Changes based on comments from finer

--- phonenumber/src/main/webapp/js/page-default.js
+++ phonenumber/src/main/webapp/js/page-default.js
@@ -1,29 +1,18 @@
 /**
  * 
  */
-var myApp = angular.module("mssp", ["ngAnimate","ngStorage"]);
+var service=angular.module("customServices",[]);
+service.factory("dataFetch",['$http',function($http){
+	      var url="datafetch?";
+	      return {
+	    	       getSingle: function(command){
+	    	    	   var promise=$http.get(url+command).success( function(response) {
+	    	    		           result=response;
+	    	    	        });
+	    	    	   return promise;
+	    	    	   }
+	    	     };
+  }
+]);
+var myApp = angular.module("mssp", ["customServices"]);
 
-angular.module("mssp").controller("tabs", ['$scope', '$http', '$q',function($scope, $http, $q) {
-	var promiseTabVariables = null;
-
-}]);
-
-angular.module("mssp").controller("global", ['$scope', '$http', '$q', '$timeout','$rootScope', function ($scope, $http, $q, $timeout,$rootScope) {
-	 $scope.initializeFlag = function(){
-		 if(localStorage.getItem("menuFlagTest")==null)
-		 {
-		 localStorage.setItem("menuFlagTest", "true");
-		 }
-		 $rootScope.flag=(localStorage.getItem("menuFlagTest")==="true");
-		 };
-		 $scope.initializeFlag();
-		 $scope.toggleflag = function(){
-		 if(localStorage.getItem("menuFlagTest")=="true")
-		 {localStorage.setItem("menuFlagTest", "false");}
-		 else{
-		 localStorage.setItem("menuFlagTest", "true");
-		 }
-		 $rootScope.flag=((localStorage.getItem("menuFlagTest")==="true"));
-		 console.log($rootScope.flag);
-		 }; 
-}]);
